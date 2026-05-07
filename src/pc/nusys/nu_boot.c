88368000f737a71847eb96083e1fa6cce63f47c6
PalindromicBreadLoaf <herbthehaircut@proton.me>
2026-05-07 09:49:42 -0400

    Create idle thread and call boot_main() in a new thread

---
+++ src/pc/nusys/nu_boot.c
@@ -0,0 +1,29 @@
+#include <SDL2/SDL.h>
+#include <PR/os_thread.h>
+#include <PR/os_host.h>
+#include <nu/nusys.h>
+
+extern void boot_main(void *);
+
+static OSThread sIdleThread;
+static OSThread sMainThread;
+static u64      sIdleStack[NU_SC_STACK_SIZE / sizeof(u64)];
+static u64      sMainStack[NU_SC_STACK_SIZE / sizeof(u64)];
+
+static void boot_idle_pc(void *arg) {
+    (void)arg;
+    nuPiInit();
+    nuScCreateScheduler(OS_VI_NTSC_LAN1, 1);
+    osCreateThread(&sMainThread, NU_MAIN_THREAD_ID, boot_main, NULL,
+                   &sMainStack[NU_SC_STACK_SIZE / sizeof(u64)], NU_MAIN_THREAD_PRI);
+    osStartThread(&sMainThread);
+
+    while (1) { SDL_Delay(100); }
+}
+
+void nuBoot(void) {
+    osInitialize();
+    osCreateThread(&sIdleThread, NU_IDLE_THREAD_ID, boot_idle_pc, NULL,
+                   &sIdleStack[NU_SC_STACK_SIZE / sizeof(u64)], 10);
+    osStartThread(&sIdleThread);
+}
